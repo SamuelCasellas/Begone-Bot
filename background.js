a464de5f5e12b886b8b13a5e8539c1dffbc39fb8
samcomon@gmail.com <sam.casellas@gmail.com>
2022-09-26 16:07:37 -0600

    Automatic popup when trial expires

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
-
 importScripts("ExtPay.js");
-const extpay = ExtPay('begone-bot')
+const extpay = ExtPay('begone-bot');
 extpay.startBackground();
 
 const millisecondsInOneDay = 86400000;
+
+const trialTimeOut = (timeTrialStartedAt) => {
+  return new Date().getTime() - timeTrialStartedAt > millisecondsInOneDay * 3
+};
 
 const authenticateUser = (paidFor=false) => {
   if (paidFor) {
@@ -12,26 +15,54 @@ const authenticateUser = (paidFor=false) => {
   chrome.storage.sync.set({authenticated: true});
 };
 
+const showTrialPageLogic = () => {
+  chrome.storage.sync.get(["shownTrialPage"], function(val) {
+    if (!val.shownTrialPage) {
+      chrome.storage.sync.set({shownTrialPage: true});
+      extpay.openTrialPage("3-day (IMPORTANT: After starting trial, please be sure to allow one minute and refresh any pages already open.)");
+    }
+  });
+};
+
+const showPaymentPageLogic = () => {
+  chrome.storage.sync.get(["shownPaymentPage"], function(val) {
+    if (!val.shownPaymentPage) {
+      chrome.storage.sync.set({shownPaymentPage: true});
+      extpay.openPaymentPage();
+    }
+  });
+};
+
 extpay.getUser().then(user => {
   if (user.paid) {
     authenticateUser(true);
   } else {
     if (!user.trialStartedAt) {
-      extpay.openTrialPage("3-day (IMPORTANT: After starting trial, please be sure to allow one minute and refresh any pages already open.)");
+      showTrialPageLogic();
     }
-    else if (new Date().getTime() - user.trialStartedAt.getTime() > millisecondsInOneDay * 3) {
+    else if (trialTimeOut(user.trialStartedAt.getTime())) {
       chrome.storage.sync.set({authenticated: false});
-      extpay.openPaymentPage();
+      showPaymentPageLogic();
     } else {
       authenticateUser();
     }
   }
-}); 
+});
 
 extpay.onPaid.addListener(user => {
+  chrome.alarms.clearAll();
   authenticateUser(true);
 });
 
 extpay.onTrialStarted.addListener(user => {
+  chrome.storage.sync.set({trialStarted: user.trialStartedAt.getTime()});
   authenticateUser();
+});
+
+chrome.storage.sync.get(["paid"], function(val) {
+  if (!val.paid) chrome.alarms.create({ delayInMinutes: 1 });
+});
+  
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.alarms.create({ delayInMinutes: 1 });
 });
