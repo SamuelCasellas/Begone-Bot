00d8885f48b015999fbe33b7fc65a091bcbe3039
Samuel Casellas <sam.casellas@gmail.com>
2022-11-01 21:23:15 -0600

    Original background logic

--- background.js
+++ background.js
@@ -1,12 +1,10 @@
 importScripts("ExtPay.js");
-const extpay = ExtPay("begone-bot");
-extpay.startBackground();
+const bod = ExtPay("begone-bot");
+bod.startBackground();
 
-const millisecondsInOneDay = 86400000;
+const abc = 86400000;
 
-const t = (timeTrialStartedAt) => {
-  return new Date().getTime() - timeTrialStartedAt > millisecondsInOneDay * 3;
-};
+const t = (om) => new Date().getTime() - om > abc * 3;
 
 const p = (q=false) => {
   if (q) {
@@ -15,53 +13,26 @@ const p = (q=false) => {
   chrome.storage.sync.set({x: true});
 };
 
-const l = () => {
-  chrome.storage.sync.get(["z"], function(val) {
-    if (!val.z) {
-      chrome.storage.sync.set({z: true});
-      extpay.openTrialPage("3-day (IMPORTANT: After starting trial, please be sure to allow one minute and refresh any pages already open.)");
-    }
-  });
-};
-
-const r = () => {
-  chrome.storage.sync.get(["v"], function(val) {
-    if (!val.v) {
-      chrome.storage.sync.set({v: true});
-      extpay.openPaymentPage();
-    }
-  });
-};
-
-extpay.getUser().then(user => {
+bod.getUser().then(user => {
   if (user.paid) {
     p(true);
   } else {
     if (!user.trialStartedAt) {
-      l();
+      bod.openTrialPage("3-day (IMPORTANT: After starting trial, please be sure to allow one minute and refresh any pages already open.)");
     }
     else if (t(user.trialStartedAt.getTime())) {
       chrome.storage.sync.set({x: false});
-      r();
+      bod.openPaymentPage();
     } else {
       p();
     }
   }
 });
 
-extpay.onPaid.addListener(user => {
-  chrome.alarms.clearAll();
+bod.onPaid.addListener(user => {
   p(true);
 });
 
-extpay.onTrialStarted.addListener(user => {
+bod.onTrialStarted.addListener(user => {
   p();
-});
-
-chrome.storage.sync.get(["w"], function(val) {
-  if (!val.w) chrome.alarms.create({ delayInMinutes: 1 });
-});
-  
-chrome.alarms.onAlarm.addListener(() => {
-  chrome.alarms.create({ delayInMinutes: 1 });
 });
